b4f7ae0c3c13e0403a154bf2d7029a331fec76f0
wenpin cui <wenpinc@vmware.com>
2015-10-06 22:04:05 +0800

    basic customize

---
+++ init.el
@@ -0,0 +1,57 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen
+(setq inhibit-startup-message t)
+
+;; keep custom settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(if (file-exists-p custom-file)
+    (load custom-file))
+
+;; are we mac os?
+(setq *is-mac* (equal system-type 'darwin))
+
+;; setup load path
+(let ((settings-dir (expand-file-name "settings" user-emacs-directory))
+      (site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory)))
+  (add-to-list 'load-path settings-dir)
+  (add-to-list 'load-path site-lisp-dir))
+
+;; setup package manager
+(require 'setup-package)
+
+(defun init-install-packages ()
+  (packages-install
+   '(magit
+     paredit
+     fill-column-indicator
+     flycheck
+     dired-details
+     yasnippet
+     smartparens
+     restclient
+     dockerfile-mode
+     )))
+
+(condition-case nil
+    (init-install-packages)
+  (error
+   (package-refresh-contents)
+   (init-install-packages)))
+
+(when *is-mac*
+  (require-package 'exec-path-from-shell)
+  (exec-path-from-shell-initialize))
+
+;; Misc
+(when *is-mac* (require 'mac))
---
+++ settings/mac.el
@@ -0,0 +1,9 @@
+;; change command to meta, and ignore option to use weird Norwegian keyboard
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+;; Don't open files from the workspace in a new frame
+(setq ns-pop-up-frames nil)
+
+(provide 'mac)
