06e77b0aecc4d6d50ea6a17ddab8ab7d48143989
wenpin cui <wcui@splunk.com>
2016-03-18 13:34:03 +0800

    modify index method of projectile to alien.

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -3,7 +3,8 @@
 (projectile-global-mode)
 
 ;; Specifies the indexing method used by Projectile.
-(setq projectile-indexing-method 'native)
+(setq projectile-indexing-method 'alien)
+(setq projectile-enable-caching t)
 
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
